3d398c705a47b104c5a011a611c9c642107c464d
Joachim Schirrmacher <j.schirrmacher@justso.de>
2018-04-23 22:10:27 +0200

    Refactored

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const grpc = require('grpc')
 const clientStub = new dgraph.DgraphClientStub('localhost:9080', grpc.credentials.createInsecure())
 const dgraphClient = new dgraph.DgraphClient(clientStub)
 
-const payment = require('./payment')
-
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
@@ -15,23 +13,13 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
 const person = require('./person')(dgraphClient)
+const network = require('./network')(dgraphClient)
+const payment = require('./payment')
 
 app.use((req, res, next) => {
   console.log(req.method, req.path)
   next()
 })
-
-async function dropAll(dgraphClient) {
-  const op = new dgraph.Operation()
-  op.setDropAll(true)
-  await dgraphClient.alter(op)
-}
-
-async function setSchema(dgraphClient) {
-  const op = new dgraph.Operation()
-  op.setSchema(`type: string @index(term) .`)
-  await dgraphClient.alter(op)
-}
 
 async function exec(func, res) {
   return func
@@ -77,44 +65,10 @@ app.post('/persons', (req, res) => exec(person.create(req.body), res))
 app.get('/persons/:id', (req, res) => exec(person.get(req.params.id), res))
 
 app.post('/tickets', (req, res) => exec(buyTicket(req.body), res))
+app.get('/account/invoices/current', (req, res) => res.json({status: 'not yet implemented'}))
 
-app.get('/network', (req, res) => {
-  const query = `{
-   all(func: anyofterms(type, "person topic")) {
-     id: uid
-     type
-     shape
-     name
-     image
-     topic {
-       uid
-     }
-   }
-  }`
-  dgraphClient.newTxn().query(query)
-    .then(data => {
-      const all = data.getJson().all
-      const links = []
-      const nodes = all.map(node => {
-        const {topic, ...result} = node
-        if (topic) {
-          topic.forEach(link => links.push({source: node.id, target: link.uid}))
-        }
-        result.details = '/person/' + result.id
-        result.visible = result.type === 'person'
-        result.open = result.name === 'XCamp'
-        return result
-      })
-      res.json({nodes, links})
-    })
-    .catch(error => res.json({error}))
-})
-
-app.delete('/network', (req, res) => {
-  dropAll(dgraphClient)
-    .then(() => setSchema(dgraphClient))
-    .then(() => res.json({}))
-})
+app.get('/network', (req, res) => exec(network.getGraph(), res))
+app.delete('/network', (req, res) => exec(network.rebuildAll(), res))
 
 const port = 8001
 app.listen(port, () => console.log('Running on port ' + port))
---
+++ network.js
@@ -0,0 +1,48 @@
+module.exports = dgraphClient => ({
+  rebuild: () => {
+    async function dropAll(dgraphClient) {
+      const op = new dgraph.Operation()
+      op.setDropAll(true)
+      await dgraphClient.alter(op)
+    }
+
+    async function setSchema(dgraphClient) {
+      const op = new dgraph.Operation()
+      op.setSchema(`type: string @index(term) .`)
+      await dgraphClient.alter(op)
+    }
+
+    return dropAll(dgraphClient).then(() => setSchema(dgraphClient))
+  },
+
+  getGraph: () => {
+    const query = `{
+      all(func: anyofterms(type, "person topic")) {
+        id: uid
+        type
+        shape
+        name
+        image
+        topic {
+          uid
+        }
+      }
+    }`
+    dgraphClient.newTxn().query(query)
+      .then(data => {
+        const all = data.getJson().all
+        const links = []
+        const nodes = all.map(node => {
+          const {topic, ...result} = node
+          if (topic) {
+            topic.forEach(link => links.push({source: node.id, target: link.uid}))
+          }
+          result.details = '/person/' + result.id
+          result.visible = result.type === 'person'
+          result.open = result.name === 'XCamp'
+          return result
+        })
+        return {nodes, links}
+      })
+  }
+})
