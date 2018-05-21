2707d503b92844ad4771580757e81b5de4b1c515
Joachim Schirrmacher <j.schirrmacher@justso.de>
2018-05-21 13:11:10 +0200

    Adapted to work in subfolders as well

--- src/person.js
+++ src/person.js
@@ -23,7 +23,7 @@ module.exports = (dgraphClient, dgraph, QueryFunction) => {
 
   async function get(txn, uid) {
     const person = await query.one(txn, `func: uid(${uid})`)
-    person.image = person.image ? '/persons/' + uid + '/picture' : '/user.png'
+    person.image = person.image ? '/netvis/persons/' + uid + '/picture' : '/user.png'
     return person
   }
 
