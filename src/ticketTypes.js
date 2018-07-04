970ee6963bf1ae786b3b70cd0e5d65ba764f045c
Joachim Schirrmacher <j.schirrmacher@justso.de>
2018-07-04 21:11:31 +0200

    Added ticket type 'sponsor'

--- src/ticketTypes.js
+++ src/ticketTypes.js
@@ -1,5 +1,6 @@
 module.exports = ticketTypes = {
   orga:      {name: 'Organizer', price: 0},
+  sponsor:   {name: 'Sponsor', price: 0},
   corporate: {name: 'Unternehmen', price: 200},
   private:   {name: 'Selbstzahler', price: 100},
   reduced:   {name: 'Reduziert', price: 50, restricted: true}
