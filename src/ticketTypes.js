2176d2df01a033a72a83659600ac5835ce07022f
Joachim Schirrmacher <j.schirrmacher@justso.de>
2019-03-10 17:36:24 +0100

    New prices

--- src/ticketTypes.js
+++ src/ticketTypes.js
@@ -2,8 +2,7 @@ module.exports = ticketTypes = {
   orga:      {name: 'Organizer', price: 0},
   speaker:   {name: 'Speaker', price: 0},
   sponsor:   {name: 'Sponsor', price: 0},
-  corporate: {name: 'Unternehmen', price: 200},
-  private:   {name: 'Selbstzahler', price: 100},
-  reduced:   {name: 'Reduziert', price: 50, restricted: true}
+  corporate: {name: 'Unternehmen', price: 250},
+  private:   {name: 'Selbstzahler (Early Bird)', price: 75},
+  reduced:   {name: 'Reduziert', price: 25, restricted: true}
 }
-
