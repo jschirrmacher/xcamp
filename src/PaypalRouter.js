d554ae225410c8108b342e8668ac088c9fc3e734
Joachim Schirrmacher <j.schirrmacher@justso.de>
2019-05-03 17:56:57 +0200

    Fixed: Router already has the path as its base - so listen on /

--- src/PaypalRouter.js
+++ src/PaypalRouter.js
@@ -1,8 +1,8 @@
 module.exports = ({express, makeHandler, Model}) => {
   const router = express.Router()
 
-  router.get('/paypal/ipn', (req, res) => res.redirect('/accounts/my', 303))
-  router.post('/paypal/ipn', makeHandler(Model.Payment.paypalIpn, {type: 'send'}))
+  router.get('/', (req, res) => res.redirect('/accounts/my', 303))
+  router.post('/', makeHandler(Model.Payment.paypalIpn, {type: 'send'}))
 
   return router
 }
