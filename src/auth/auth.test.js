df840698a3949d0928d5312b22f8e62cef395a8c
Joachim Schirrmacher <j.schirrmacher@justso.de>
2019-05-30 12:27:59 +0200

    Added test to make sure that JWT is generated and set as a cookie

--- src/auth/auth.test.js
+++ src/auth/auth.test.js
@@ -49,22 +49,31 @@ const config = {
 
 const auth = require('.')({app, Model, dgraphClient, readModels, store, config})
 
-function expectResult(status, json) {
+const emptyFunc = () => {}
+const expect = expected => data => data.should.deepEqual(expected)
+
+function expectValidCookie() {
+  return (name, value) => {
+    name.should.equal('token')
+    jwt.verify(value, config.authSecret, (err, decoded) => {
+      should(err).be.null()
+      decoded.should.have.properties(['iat', 'exp'])
+    })
+  }
+}
+
+function makeExpectedResult({status = 200, json = emptyFunc, cookie = emptyFunc}) {
   return res = {
-    status: wrap(status),
-    json: wrap(json),
-    cookie(name, value) {
-      name.should.equal('token')
-      jwt.verify(value, config.authSecret, (err, decoded) => {
-        should(err).be.null()
-        decoded.should.have.properties(['iat', 'exp'])
-      })
-    }
+    status: wrap(status, 'status'),
+    json: wrap(json, 'json'),
+    cookie: wrap(cookie, 'cookie'),
+    called: {status: false, json: false, cookie: false}
   }
 
-  function wrap(func) {
-    return data => {
-      func(data)
+  function wrap(func, called) {
+    return (...args) => {
+      res.called[called] = true
+      func(...args)
       return res
     }
   }
@@ -74,7 +83,7 @@ describe('auth', () => {
   it('should authenticate with e-mail and password', done => {
     const middleware = auth.requireLogin()
     const req = {body: {email: 'test@example.com', password: 'test-pwd'}}
-    const res = expectResult(code => code.should.equal(200))
+    const res = makeExpectedResult({status: expect(200)})
     middleware(req, res, err => {
       should(err).be.undefined()
       req.user.should.have.property('id')
@@ -83,13 +92,23 @@ describe('auth', () => {
     })
   })
 
+  it('should generate a JWT if authenticated with e-mail and password', done => {
+    const middleware = auth.requireLogin()
+    const req = {body: {email: 'test@example.com', password: 'test-pwd'}}
+    const res = makeExpectedResult({cookie: expectValidCookie()})
+    middleware(req, res, err => {
+      res.called.cookie.should.be.true()
+      done()
+    })
+  })
+
   it('should not authenticate with e-mail and wrong password', done => {
     const middleware = auth.requireLogin()
     const req = {body: {email: 'test@example.com', password: 'wrong-pwd'}}
-    const res = expectResult(
-      code => code.should.equal(401),
-      data => data.should.deepEqual({error: 'Not authenticated'})
-    )
+    const res = makeExpectedResult({
+      status: expect(401),
+      json: expect({error: 'Not authenticated'})
+    })
     middleware(req, res, () => should().fail())
     done()
   })
@@ -97,7 +116,7 @@ describe('auth', () => {
   it('should authenticate with access code and hash', done => {
     const middleware = auth.requireCodeAndHash()
     const req = {body: {email: 'test3@example.com'}, params: {accessCode: 'test-access', hash: 'test-hash'}}
-    const res = expectResult(code => code.should.equal(200))
+    const res = makeExpectedResult({status: expect(200)})
     middleware(req, res, err => {
       should(err).be.undefined()
       req.user.should.have.property('id')
@@ -110,7 +129,7 @@ describe('auth', () => {
     const middleware = auth.requireJWT()
     const authorization = jwt.sign({sub: 4712}, config.authSecret, {expiresIn: '24h'})
     const req = {body: {email: 'test3@example.com'}, headers: {authorization}}
-    const res = expectResult(code => code.should.equal(200))
+    const res = makeExpectedResult({status: expect(200)})
     middleware(req, res, err => {
       should(err).be.undefined()
       req.user.should.have.property('id')
