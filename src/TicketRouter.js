0c5ae388b8154a0511ddcbc488bca0549dcbe1f0
Joachim Schirrmacher <j.schirrmacher@justso.de>
2019-06-16 17:17:33 +0200

    Removed unused dependeny of TicketRouter

--- src/TicketRouter.js
+++ src/TicketRouter.js
@@ -5,7 +5,6 @@ module.exports = (dependencies) => {
     makeHandler,
     templateGenerator,
     mailSender,
-    mailChimp,
     Model,
     store,
     readModels,
--- src/mainRouter.js
+++ src/mainRouter.js
@@ -66,7 +66,7 @@ module.exports = (dependencies) => {
   const sessionRouter = require('./SessionRouter')({express, auth, makeHandler, templateGenerator, Model, readModels, config})
   const newsletterRouter = require('./NewsletterRouter')({express, auth, makeHandler, templateGenerator, mailSender, mailChimp, Model, store})
   const accountsRouter = require('./AccountsRouter')({express, auth, makeHandler, templateGenerator, mailSender, Model, store, readModels, config})
-  const ticketRouter = require('./TicketRouter')({express, auth, makeHandler, templateGenerator, mailSender, mailChimp, Model, store, readModels, config})
+  const ticketRouter = require('./TicketRouter')({express, auth, makeHandler, templateGenerator, mailSender, Model, store, readModels, config})
   const networkRouter = require('./NetworkRouter')({express, auth, makeHandler, templateGenerator, Model, store, readModels})
   const orgaRouter = require('./OrgaRouter')({express, auth, makeHandler, templateGenerator, mailSender, Model, readModels, store, config })
   const paypalRouter = require('./PaypalRouter')({express, makeHandler, Model})
