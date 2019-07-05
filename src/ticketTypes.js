8a9a883fb3231ebde260cbb4c008a45e81a5010c
Joachim Schirrmacher <j.schirrmacher@justso.de>
2019-07-05 05:54:53 +0200

    Fixed: Count tickets correctly in invoices list summary

--- src/ticketTypes.js
+++ src/ticketTypes.js
@@ -1,12 +1,12 @@
 const config = require('../config/config')
 
 module.exports = {
-  orga:      {name: 'Organizer', price: 0},
-  speaker:   {name: 'Speaker', price: 0},
-  sponsor:   {name: 'Sponsor', price: 0},
-  corporate: {name: 'Unternehmen', price: config.ticketCategories.corporate},
-  earlybird: {name: 'Early Bird', price: config.ticketCategories.earlybird, restricted: true},
-  private:   {name: 'Selbstzahler', price: config.ticketCategories.private},
-  lastminute:{name: 'Last Minute', price: config.ticketCategories.lastminute},
-  reduced:   {name: 'Reduziert', price: config.ticketCategories.reduced, restricted: true}
+  orga:      {category: 'free', name: 'Organizer', price: 0},
+  speaker:   {category: 'free', name: 'Speaker', price: 0},
+  sponsor:   {category: 'free', name: 'Sponsor', price: 0},
+  corporate: {category: 'corporate', name: 'Unternehmen', price: config.ticketCategories.corporate},
+  earlybird: {category: 'private', name: 'Early Bird', price: config.ticketCategories.earlybird, restricted: true},
+  private:   {category: 'private', name: 'Selbstzahler', price: config.ticketCategories.private},
+  lastminute:{category: 'private', name: 'Last Minute', price: config.ticketCategories.lastminute},
+  reduced:   {category: 'reduced', name: 'Reduziert', price: config.ticketCategories.reduced, restricted: true}
 }
