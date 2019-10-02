98a65c0589bfd68d9b94229c663bc988568be667
Joachim Schirrmacher <j.schirrmacher@justso.de>
2019-10-02 21:31:57 +0200

    Fixed linting errors

--- src/test/ContentReader.test.js
+++ src/test/ContentReader.test.js
@@ -1,21 +1,21 @@
-const should = require('should')
+require('should')
 const ContentReader = require('../ContentReader')
 
 const reader = ContentReader({logger: console, config: {basePath: __dirname}})
 
 describe('ContentReader', () => {
   it('should return meta information', () => {
-    const {html, meta} = reader.getPageContent('test-page', 'blog', '_posts')
+    const {meta} = reader.getPageContent('test-page', 'blog', '_posts')
     meta.should.containDeep({title: 'Test page', layout: 'test-layout'})
   })
 
   it('should render blog page content', () => {
-    const {html, meta} = reader.getPageContent('test-page', 'blog', '_posts')
+    const {html} = reader.getPageContent('test-page', 'blog', '_posts')
     html.should.match(/<p>Content of test page<\/p>/)
   })
 
   it('should resolve internal links correctly', () => {
-    const {html, meta} = reader.getPageContent('test-page', 'blog', '_posts')
+    const {html} = reader.getPageContent('test-page', 'blog', '_posts')
     html.should.match(/a href="blog\/test-page#anchor"/)
   })
 })
